9356609cc8e04966743f5bb5806d685dc2058bc1
tusong <1647524190@qq.com>
2025-03-22 16:05:20 +0800

    add adaptive pool

--- AdaptivePool.py
+++ AdaptivePool.py
@@ -1,15 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from test_gusssian import padding
 
 
 class AdaptivePool(nn.Module):
     def __init__(self, kernel, stride, padding=0):
         """
-        自适应池化：在 kernel_size x kernel_size 区域内动态选择最大池化或均值池化
+        自适应池化
         Args:
-            kernel_size (int): 池化窗口大小
+            kernel (int): 池化窗口大小
             stride (int): 步长
             padding(int): 填充
         """
@@ -18,16 +17,12 @@ class AdaptivePool(nn.Module):
         self.stride = stride
         self.padding = padding
 
-        self.weight_conv = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1, stride=1, bias=True)
-
     def forward(self, x):
-        # 计算 MaxPool 和 AvgPool
         max_pool = F.max_pool2d(x, kernel_size=self.kernel, stride=self.stride, padding=self.padding)
         avg_pool = F.avg_pool2d(x, kernel_size=self.kernel, stride=self.stride, padding=self.padding)
 
-        # x = torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
         x = x.mean(dim=1, keepdim=True)
-        weight = torch.sigmoid(self.weight_conv(x))
+        weight = torch.sigmoid(x)
 
         out = weight * max_pool + (1 - weight) * avg_pool
         return out
@@ -38,4 +33,3 @@ if __name__ == "__main__":
     x = torch.randn(1, 3, 8, 8)
     model = AdaptivePool(kernel=2, stride=2)
     output = model(x)
-
